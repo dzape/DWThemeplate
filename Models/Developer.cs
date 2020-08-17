9800764297ce20b9cc2bdbddada0e12606f2a85b
dzape <p.nikolic893@outlook.com>
2020-08-17 15:21:10 +0200

    first commit

---
+++ Models/Developer.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DWThemeplate.Models
+{ 
+    public class Developer
+    {
+        public String FullName { get; set; }
+        public String UserName { get; set; }
+        public int Years { get; set; }
+        public String About { get; set; }
+    }
+}
---
+++ Models/SocialMedia.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DWThemeplate.Models
+{
+    // You can add your own \_(^.^)_/
+    public class SocialMedia
+    {
+        public String Mail { get; set; }
+        public String GitHub { get; set; }
+        public String LinkedIn { get; set; }
+        public String Instagram { get; set; } 
+    }
+}
